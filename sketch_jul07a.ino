839eedceb38fd7c10ae43034a93d2f8da433f628
Mahezer Carvalho <mahezercarvalho@gmail.com>
2018-09-13 01:03:23 +0000

    Consume REST API

--- sketch_jul07a.ino
+++ sketch_jul07a.ino
@@ -2,6 +2,8 @@
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define DHTPIN 0
 #define DHTTYPE DHT11
@@ -13,8 +15,8 @@ const int energy_lm35 = D2;
 DHT dht(DHTPIN, DHTTYPE);
 float leitura = 0;
 
-char* ssid = "";
-char* password = "";
+const char* ssid = "AndroidAP";
+const char* password = "daim9608";
 
 const char* mqttServer = "m10.cloudmqtt.com";
 const int mqttPort = 15026;
@@ -66,31 +68,59 @@ void setup() {
 }
  
 void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http; //Object of class HTTPClient
+    http.begin("http://jsonplaceholder.typicode.com/users/1");
+    int httpCode = http.GET();
+
+    if (httpCode > 0) 
+    {
+      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+      JsonObject& root = jsonBuffer.parseObject(http.getString());
+      // Parameters
+      int id = root["id"]; // 1
+      const char* name = root["name"]; // "Leanne Graham"
+      const char* username = root["username"]; // "Bret"
+      const char* email = root["email"]; // "Sincere@april.biz"
+      // Output to serial monitor
+      Serial.print("Name:");
+      Serial.println(name);
+      Serial.print("Username:");
+      Serial.println(username);
+      Serial.print("Email:"); 
+      Serial.println(email);
+    }
+    http.end(); //Close connection
+  }
+
+  
   digitalWrite(energy_ldr, HIGH);
   leitura = analogRead(sensor);
   delay(50);
-  snprintf(mqttMessage, 40, "LDR = %.2f", leitura);
-  if (client.publish("esp/test", mqttMessage)) {
+  snprintf(mqttMessage, 40, "%.2f", leitura);
+  if (client.publish("ldr", mqttMessage)) {
     Serial.print("LDR = ");
     Serial.println(leitura);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
-    client.publish("esp/test", mqttMessage);
+    client.publish("ldr", mqttMessage);
   }
   digitalWrite(energy_ldr, LOW);
   digitalWrite(energy_lm35, HIGH);
   leitura = ((analogRead(sensor)/1024.0)*3300)/10;
   Serial.print("LM35 - ");
   Serial.println(leitura);
-  snprintf(mqttMessage, 40, "LM35 = %.2f", leitura);
-  if (client.publish("esp/test", mqttMessage)) {
+  snprintf(mqttMessage, 40, "%.2f", leitura);
+  if (client.publish("lm35", mqttMessage)) {
     Serial.print("LM35 = ");
     Serial.println(leitura);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
-    client.publish("esp/test", mqttMessage);
+    client.publish("lm35", mqttMessage);
   }
   digitalWrite(energy_lm35, LOW);
   digitalWrite(energy_dht11, HIGH);
@@ -102,14 +132,14 @@ void loop() {
       Serial.print("DHT11: ");
       Serial.println(h);
   }
-  snprintf(mqttMessage, 40, "DHT11 = %.2f", h);
-  if (client.publish("esp/test", mqttMessage)) {
+  snprintf(mqttMessage, 40, "%.2f", h);
+  if (client.publish("dht11", mqttMessage)) {
     Serial.print("DHT11 = ");
     Serial.println(mqttMessage);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
-    client.publish("esp/test", mqttMessage);
+    client.publish("dht11", mqttMessage);
   }
   digitalWrite(energy_dht11, LOW);
   delay(1000);
