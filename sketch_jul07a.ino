08207016919d217c4e0665488c978bbca79e8e0d
Mahezer Carvalho <mahezercarvalho@gmail.com>
2018-09-15 16:22:54 -0300

    Finish the light shows

--- sketch_jul07a.ino
+++ sketch_jul07a.ino
@@ -12,17 +12,25 @@ const int sensor = A0;     // pino de leitura digital do sensor
 const int energy_ldr = D0; 
 const int energy_dht11 = D1; 
 const int energy_lm35 = D2; 
+const int light_ldr = D5;
+const int light_lm35 = D6;
+const int light_dht11 = D7;
+
 DHT dht(DHTPIN, DHTTYPE);
 float leitura = 0;
 
-const char* ssid = "AndroidAP";
-const char* password = "daim9608";
+const char* ssid = "Mehida21";
+const char* password = "Sandoval21";
 
 const char* mqttServer = "m10.cloudmqtt.com";
 const int mqttPort = 15026;
 const char* mqttUser = "qxuufkxb";
 const char* mqttPassword = "iz1cz4OG_42S";
 const char* mqttClientId = "Roland";
+
+float ldr_config;
+float dht11_config;
+float lm35_config;
 
 char mqttMessage[40];
 
@@ -62,35 +70,35 @@ void setup() {
   pinMode(energy_ldr, OUTPUT);
   pinMode(energy_dht11, OUTPUT);
   pinMode(energy_lm35, OUTPUT);
+  pinMode(light_ldr, OUTPUT);
+  pinMode(light_dht11, OUTPUT);
+  pinMode(light_lm35, OUTPUT);
   pinMode(sensor, INPUT);
 
   dht.begin();
 }
  
 void loop() {
-
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http; //Object of class HTTPClient
-    http.begin("http://jsonplaceholder.typicode.com/users/1");
+    http.begin("http://aqueous-beach-59482.herokuapp.com/config");
     int httpCode = http.GET();
-
     if (httpCode > 0) 
     {
       const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
       DynamicJsonBuffer jsonBuffer(bufferSize);
       JsonObject& root = jsonBuffer.parseObject(http.getString());
-      // Parameters
-      int id = root["id"]; // 1
-      const char* name = root["name"]; // "Leanne Graham"
-      const char* username = root["username"]; // "Bret"
-      const char* email = root["email"]; // "Sincere@april.biz"
+      // Parameter
+      ldr_config = (float)root["ldr"]; // "Leanne Graham"
+      dht11_config = (float)root["dht11"]; // "Bret"
+      lm35_config = (float)root["lm35"]; // "Sincere@april.biz"
       // Output to serial monitor
       Serial.print("Name:");
-      Serial.println(name);
+      Serial.println(ldr_config);
       Serial.print("Username:");
-      Serial.println(username);
+      Serial.println(dht11_config);
       Serial.print("Email:"); 
-      Serial.println(email);
+      Serial.println(lm35_config);
     }
     http.end(); //Close connection
   }
@@ -98,11 +106,16 @@ void loop() {
   
   digitalWrite(energy_ldr, HIGH);
   leitura = analogRead(sensor);
+  if(leitura > ldr_config) {
+    digitalWrite(light_ldr, HIGH);
+  } else {
+    digitalWrite(light_ldr, LOW);
+  }
   delay(50);
   snprintf(mqttMessage, 40, "%.2f", leitura);
   if (client.publish("ldr", mqttMessage)) {
-    Serial.print("LDR = ");
-    Serial.println(leitura);
+  //  Serial.print("LDR = ");
+  //  Serial.println(leitura);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
@@ -110,13 +123,18 @@ void loop() {
   }
   digitalWrite(energy_ldr, LOW);
   digitalWrite(energy_lm35, HIGH);
-  leitura = ((analogRead(sensor)/1024.0)*3300)/10;
-  Serial.print("LM35 - ");
-  Serial.println(leitura);
+  leitura = ((analogRead(sensor)/1024.0)*5000)/10;
+  if (leitura > lm35_config) {
+    digitalWrite(light_lm35, HIGH);
+  } else {
+    digitalWrite(light_lm35, LOW);
+  }
+  //Serial.print("LM35 - ");
+  //Serial.println(leitura);
   snprintf(mqttMessage, 40, "%.2f", leitura);
   if (client.publish("lm35", mqttMessage)) {
-    Serial.print("LM35 = ");
-    Serial.println(leitura);
+    //Serial.print("LM35 = ");
+    //Serial.println(leitura);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
@@ -129,13 +147,18 @@ void loop() {
   if (isnan(h)) {
     Serial.println("Failed to read from DHT sensor!");
   } else {
-      Serial.print("DHT11: ");
-      Serial.println(h);
+     // Serial.print("DHT11: ");
+     // Serial.println(h);
+  }
+  if (h < dht11_config) {
+    digitalWrite(light_dht11, HIGH);
+  } else {
+    digitalWrite(light_dht11, LOW);
   }
   snprintf(mqttMessage, 40, "%.2f", h);
   if (client.publish("dht11", mqttMessage)) {
-    Serial.print("DHT11 = ");
-    Serial.println(mqttMessage);
+    //Serial.print("DHT11 = ");
+    //Serial.println(mqttMessage);
   } else {
     client.connect(mqttClientId, mqttUser, mqttPassword);
     delay(10); 
